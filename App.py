1f6d05d1f4328a35357f8cdcd937345639f5f96a
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-04-25 03:20:59 +0300

    25/04/21

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import messagebox, ttk
+from tkinter import messagebox, ttk, filedialog
 import pyrebase
 import cv2
 from pathlib import Path
@@ -9,7 +9,10 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
+import pandas as pd
+import numpy as np
 import webbrowser
+from PIL import ImageTk, Image
 
 firebaseConfig = {
     "apiKey": "AIzaSyCGHyT8asvQwZUGZtpIbTYRozvLYaHBqPo",
@@ -31,10 +34,6 @@ auth1 = firebase.auth()
 encodelist = ()
 
 
-# root = Tk()
-# root.geometry("1100x700")
-
-
 class FaceNet(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -47,7 +46,9 @@ class FaceNet(tk.Tk):
 
         self.frames = {}
 
-        for F in (Login, StudentRegister, TeacherRegister, ForgotPassword):
+        for F in (
+                Login, StudentRegister, TeacherRegister, ForgotPassword, TeacherMainPage, StudentMainPage,
+                CreateCourse):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -62,7 +63,7 @@ class FaceNet(tk.Tk):
 
 class Login(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg="#414141")
 
         def login():
             if str(email.get()).isspace() or str(password.get()).isspace():
@@ -74,8 +75,13 @@ class Login(tk.Frame):
             else:
                 try:
                     User = auth.get_user_by_email(str(email.get())).email_verified
+
                     if User:
-                        print(auth1.sign_in_with_email_and_password(str(email.get()), str(password.get())))
+                        auth1.sign_in_with_email_and_password(str(email.get()), str(password.get()))
+                        if str(email.get()).endswith("@isikun.edu.tr") or str(email.get()) == "korhan.koz@isik.edu.tr":
+                            controller.show_frame(TeacherMainPage)
+                        else:
+                            controller.show_frame(StudentMainPage)
                     else:
                         messagebox.showerror("Email Not Verified or Not Registered", "Please check your email")
                 except:
@@ -99,17 +105,17 @@ class Login(tk.Frame):
             password.insert(0, "Password")
             controller.show_frame(TeacherRegister)
 
-        welcome = Label(self, text=" SIGN IN ", width=150, height=5)
+        welcome = Label(self, text=" SIGN IN ", width=150, height=10, bg="#414141", fg="#FFFFFF")
         welcome.grid(row=3, column=1, columnspan=3, padx=10, pady=10)
 
-        emailL = Label(self, text="E-mail:", width=10, height=2)
-        emailL.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
+        emailL = Label(self, text="E-mail:", width=10, height=2, bg="#313131", fg="#FFFFFF")
+        emailL.grid(row=5, column=0, columnspan=3, padx=10, pady=20)
 
         email = Entry(self, width=50, borderwidth=5, bg="#72A4D2", fg="#FFFFFF")
         email.grid(row=5, column=1, columnspan=3, padx=10, pady=10)
         email.insert(0, "Email")
 
-        passwordL = Label(self, text="Password:", width=10, height=2)
+        passwordL = Label(self, text="Password:", width=10, height=2, bg="#313131", fg="#FFFFFF")
         passwordL.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
 
         password = Entry(self, width=50, borderwidth=5, bg="#72A4D2", fg="#FFFFFF", show="*")
@@ -118,23 +124,24 @@ class Login(tk.Frame):
 
         refresh()
 
-        forgotPassword = ttk.Button(self, text="Forgot your password?", cursor="hand2",
-                                    command=lambda: controller.show_frame(ForgotPassword))
+        forgotPassword = Button(self, text="Forgot your password?", cursor="hand2",
+                                command=lambda: controller.show_frame(ForgotPassword), bg="#ca3e47", fg="#FFFFFF",
+                                width=20)
         forgotPassword.grid(row=23, column=1, columnspan=3, padx=10, pady=10)
 
-        myButton = Button(self, text="Sign In!", command=login, fg="white", bg="orange")
-        myButton.grid(row=17, column=1, columnspan=3, padx=10, pady=10)
+        myButton = Button(self, text="Sign In!", command=login, fg="white", bg="#ca3e47", width=10)
+        myButton.grid(row=17, column=1, columnspan=3, padx=10, pady=20)
 
-        buttonStudent = ttk.Button(self, text="Student Sign Up", command=backS)
+        buttonStudent = Button(self, text="Student Sign Up", command=backS, bg="#325288", fg="#FFFFFF", width=15)
         buttonStudent.grid(row=21, column=0, columnspan=3, padx=5, pady=5)
 
-        buttonTeacher = ttk.Button(self, text="Teacher Sign Up", command=backT)
+        buttonTeacher = Button(self, text="Teacher Sign Up", command=backT, bg="#325288", fg="#FFFFFF", width=15)
         buttonTeacher.grid(row=21, column=2, columnspan=3, padx=5, pady=5)
 
 
 class StudentRegister(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg="#414141")
 
         def register():
             if str(name.get()).isspace() or str(surname.get()).isspace() or str(email.get()).isspace() or str(
@@ -250,7 +257,7 @@ class StudentRegister(tk.Frame):
             passwordA.insert(0, "Password")
             controller.show_frame(Login)
 
-        welcome = Label(self, text=" STUDENT SIGN UP ", width=150, height=5)
+        welcome = Label(self, text=" STUDENT SIGN UP ", width=150, height=5, bg="#414141", fg="#FFFFFF")
         welcome.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
         name = Entry(self, width=50, borderwidth=5, bg="#72A4D2", fg="#FFFFFF")
@@ -279,16 +286,16 @@ class StudentRegister(tk.Frame):
 
         refresh()
 
-        myButton = Button(self, text="Sign Up!", command=register, fg="white", bg="orange")
+        myButton = Button(self, text="Sign Up!", command=register, fg="white", bg="#ca3e47", width=10)
         myButton.grid(row=15, column=1, columnspan=3, padx=10, pady=10)
 
-        buttonBack = ttk.Button(self, text="Back", command=back)
+        buttonBack = Button(self, text="Back", command=back, bg="#fed049", width=10)
         buttonBack.grid(row=15, column=0, columnspan=3, padx=5, pady=5)
 
 
 class TeacherRegister(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg="#414141")
 
         def register():
             if str(name.get()).isspace() or str(surname.get()).isspace() or str(email.get()).isspace() or str(
@@ -349,7 +356,7 @@ class TeacherRegister(tk.Frame):
             passwordA.insert(0, "Password")
             controller.show_frame(Login)
 
-        welcome = Label(self, text=" TEACHER SIGN UP ", width=150, height=5)
+        welcome = Label(self, text=" TEACHER SIGN UP ", width=150, height=5, bg="#414141", fg="#FFFFFF")
         welcome.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
         name = Entry(self, width=50, borderwidth=5, bg="#72A4D2", fg="#FFFFFF")
@@ -374,16 +381,16 @@ class TeacherRegister(tk.Frame):
 
         refresh()
 
-        myButton = Button(self, text="Sign Up!", command=register, fg="white", bg="orange")
+        myButton = Button(self, text="Sign Up!", command=register, fg="white", bg="#ca3e47", width=10)
         myButton.grid(row=15, column=1, columnspan=3, padx=10, pady=10)
 
-        buttonBack = ttk.Button(self, text="Back", command=back)
+        buttonBack = Button(self, text="Back", command=back, bg="#fed049", width=10)
         buttonBack.grid(row=15, column=0, columnspan=3, padx=5, pady=5)
 
 
 class ForgotPassword(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg="#414141")
 
         def mail():
             try:
@@ -400,7 +407,7 @@ class ForgotPassword(tk.Frame):
             email.insert(0, "Enter your email")
             controller.show_frame(Login)
 
-        welcome = Label(self, text=" FORGOT PASSWORD ", width=150, height=5)
+        welcome = Label(self, text=" FORGOT PASSWORD ", width=150, height=5, bg="#414141", fg="#FFFFFF")
         welcome.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
         email = Entry(self, width=50, borderwidth=5, bg="#72A4D2", fg="#FFFFFF")
@@ -409,12 +416,142 @@ class ForgotPassword(tk.Frame):
 
         refresh()
 
-        buttonBack = ttk.Button(self, text="Back", command=back)
+        buttonBack = Button(self, text="Back", command=back, bg="#fed049", width=10)
         buttonBack.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
 
-        myButton = Button(self, text="Submit", command=mail, fg="white", bg="orange")
+        myButton = Button(self, text="Submit", command=mail, fg="white", bg="#ca3e47", width=10)
         myButton.grid(row=5, column=1, columnspan=3, padx=10, pady=10)
 
 
+class TeacherMainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        frameHeader = Frame(self, height=100, width=1350, bg="#313131", padx=20, relief=SUNKEN, borderwidth=2)
+        frameHeader.pack(side=TOP, fill=X)
+
+        # logo = ImageTk.PhotoImage(Image.open("images/facenet.png"))
+        # logoLabel = Label(frameHeader, image=logo)
+        # logoLabel.pack(side=LEFT)
+
+        logoutButton = Button(frameHeader, text="Logout", command=lambda: controller.show_frame(Login), width=10,
+                              bg="#ca3e47", fg="#FFFFFF")
+        logoutButton.pack(side=RIGHT)
+
+        welcomeMessage = "Welcome " + str(auth1.current_user)
+
+        nameLabel = Label(frameHeader, height=6, width=50, bg="#313131", text=welcomeMessage, fg="#FFFFFF")
+        nameLabel.pack(side=RIGHT)
+
+        frameCenter = Frame(self, width=1350, relief=RIDGE, bg="#414141", height=680)
+        frameCenter.pack(side=TOP, fill=X)
+
+        frameButtons = Frame(frameCenter, height=700, width=900, bg="#414141", borderwidth=2, relief=SUNKEN)
+        frameButtons.pack(side=LEFT)
+
+        buttonCC = Button(frameButtons, text="Create Course", command=lambda: controller.show_frame(CreateCourse),
+                          width=13, bg="#ca3e47", fg="#FFFFFF")
+        buttonCC.grid(row=1, column=1, columnspan=3, padx=10, pady=40)
+
+        buttonDC = Button(frameButtons, text="Delete Course", command=lambda: controller.show_frame(CreateCourse),
+                          width=13, bg="#ca3e47", fg="#FFFFFF")
+        buttonDC.grid(row=3, column=1, columnspan=3, padx=10, pady=40)
+
+        buttonCE = Button(frameButtons, text="Create Exam", command=lambda: controller.show_frame(CreateCourse),
+                          width=13, bg="#ca3e47", fg="#FFFFFF")
+        buttonCE.grid(row=5, column=1, columnspan=3, padx=10, pady=40)
+
+        buttonDE = Button(frameButtons, text="Delete Exam", command=lambda: controller.show_frame(CreateCourse),
+                          width=13, bg="#ca3e47", fg="#FFFFFF")
+        buttonDE.grid(row=7, column=1, columnspan=3, padx=10, pady=40)
+
+        frameCourses = Frame(frameCenter, height=700, width=1350, bg="#414141", borderwidth=2, relief=SUNKEN)
+        frameCourses.pack(side=LEFT, fill=X)
+
+
+class StudentMainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        frame = Frame(self, height=100, width=300, bg="#313131", bd='5', relief=SUNKEN)
+        frame.grid()
+
+
+class CreateCourse(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg="#414141")
+
+        def back():
+            self.documentLabel.configure(text="File Name")
+            controller.show_frame(TeacherMainPage)
+
+        welcome = Label(self, text=" ADD COURSE ", width=150, height=10, bg="#414141", fg="#FFFFFF")
+        welcome.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
+
+        backButton = Button(self, text="Back", command=back,
+                            width=10, bg="#fed049")
+        backButton.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+
+        browseButton = Button(self, text="Browse a File", command=self.fileDialog,
+                              width=10, bg="#ca3e47", fg="#FFFFFF")
+        browseButton.grid(row=3, column=1, columnspan=3, padx=10, pady=10)
+
+        self.documentLabel = Label(self, text="File Name", width=30)
+        self.documentLabel.grid(row=3, column=2, columnspan=3, padx=10, pady=10)
+
+        buttonSubmit = Button(self, text="Submit Classes", width=13, bg="#ca3e47", fg="#FFFFFF")
+        buttonSubmit.grid(row=5, column=1, columnspan=3, padx=10, pady=10)
+
+    def fileDialog(self):
+        self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=
+        (("csv files", "*.csv"), ("all files", "*.*")))
+        self.documentLabel.configure(text=self.filename)
+
+        if len(self.filename) == 0:
+            messagebox.showinfo("Empty File", "Please add a csv file.")
+            self.documentLabel.configure(text="File Name")
+
+        elif not str(self.filename).endswith(".csv"):
+            messagebox.showinfo("Wrong Type", "Check your file type.")
+            self.documentLabel.configure(text="File Name")
+
+        else:
+            raw_data = pd.read_csv(f'{self.filename}')
+            asarray = np.asarray(raw_data["StudentID"])
+
+    def createCourses(courseName, courseID, TeacherMail):
+        courseQu = db.child("courses").shallow().get()
+        if (courseQu.val() == None):
+            raw_data = pd.read_csv(r'C:\Users\KORHAN.KOZ\Desktop\asddsa.csv')
+            print(raw_data.head(15))
+            asarray = np.asarray(raw_data["StudentID"])
+
+            courseData = {"CourseName": courseName, "TeacherMail": TeacherMail}
+            db.child("courses").child(courseID).set(courseData)
+            enrollData = {"takesCourse": True}
+            for student in asarray:
+                db.child("coursesENROLL").child(courseID).child(student).set(enrollData)
+        else:
+            courseCodesArray = list(courseQu.val())
+            print(courseCodesArray)
+
+            if courseID not in courseCodesArray:
+                raw_data = pd.read_csv(r'C:\Users\KORHAN.KOZ\Desktop\asddsa.csv')
+                print(raw_data.head(15))
+                asarray = np.asarray(raw_data["StudentID"])
+
+                courseData = {"CourseName": courseName, "TeacherMail": TeacherMail}
+                db.child("courses").child(courseID).set(courseData)
+                enrollData = {"takesCourse": True}
+                for student in asarray:
+                    db.child("coursesENROLL").child(courseID).child(student).set(enrollData)
+            else:
+                print("Course exists")
+
+
 app = FaceNet()
+app.geometry("1050x600")
+app.title("FaceNet")
+# app.iconbitmap('images/faceprint.jgp') It must end with .ico
+# root.geometry("1100x700")
 app.mainloop()
